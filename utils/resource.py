4099f2b68c07f0ff955eba991e22d1de4aa75b79
omugale2004 <omugale3654@gmail.com>
2024-06-26 00:31:29 +0530

    Delete utils directory

--- utils/resource.py
+++
@@ -1,8 +0,0 @@
-import flask_restful as restful
-
-class BaseResource(restful.Resource):
-    method_decorators = []
-
-
-    def options(self, *args, **kwargs):
-        return {"status": "OPTIONS OK"}
